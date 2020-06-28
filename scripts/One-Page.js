c96db51e7cbf45c1928880cdfdfdefbf5aa0c53e
Dennis Naujokat - Tower <prinzdarknis@googlemail.de>
2020-06-28 14:59:08 +0200

    OnePage Entwurf

---
+++ scripts/One-Page.js
@@ -0,0 +1,30 @@
+$(function(){
+/*
+    var containers = [
+        [$( ".main" ),      $( "#main_pos" ),    ],
+        [$( ".lsf" ),       $( "#lsf_pos" ),     ],
+        [$( ".sim" ),       $( "#sim_pos" ),     ],
+        [$( ".fhem" ),      $( "#fhem_pos" ),    ],
+        [$( ".tablet" ),    $( "#tablet_pos" ),  ],
+        [$( ".team" ),      $( "#team_pos" ),    ],
+        [$( ".contact" ),   $( "#contact_pos" ), ],
+        [$( ".impressum" ), $( "#impressum_pos" )],
+        [$( ".dsgvo" ),     $( "#dsgvo_pos" ),   ]
+    ];
+
+    //Laden und Entladen
+    var preload = 20;
+    window.onscroll = function() {
+        containers.forEach(function(item, index, array) {
+            var elementOffsetTop = item[1].offset().top;
+            var windowOffsetBottom = $(window).scrollTop() + $(window).height();
+            if (elementOffsetTop < windowOffsetBottom + preload) {
+                item[0].show();
+            }
+            else {
+                item[0].hide();
+            }
+          });
+    }  
+*/
+});
